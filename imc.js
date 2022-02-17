dc41aca84dc1b6fcc34564296ad14a75dca9d64e
Adrio <adrio.pegoraro@yahoo.com.br>
2022-02-17 16:31:06 -0300

    configurando os parâmetros  do if

--- imc.js
+++ imc.js
@@ -1,3 +1,9 @@
 var altura = prompt('Insira sua altura!')
 var peso = prompt('insira seu peso!')
 imc = peso / (altura * altura)
+
+if (peso < 18.5) alert("Seu IMC é " + imc + "! " + "você está abaixo do peso!")
+else if (peso <= 24.9) alert("Seu IMC é " + imc +"! " + "Peso normal!")
+else if (peso <= 29.9) alert("Seu IMC é " + imc +"! " + "Sobrepeso grau 1!")
+else if (peso <= 39.9) alert("Seu IMC é " + imc +"! " + "Obesidade grau 2!")
+else alert("Seu IMC é " + imc +"! " + "Obesidade grave grau 3!")
